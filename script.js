e61a130e917c676ad6bf07d99bf23094f01d5e05
Evgeniy <85104881+EVG777-prog@users.noreply.github.com>
2021-09-18 21:38:03 +0300

    init: start cv-task

---
+++ script.js
@@ -0,0 +1,81 @@
+'use strict'
+
+const isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows());
+    }
+};
+
+
+if (isMobile.any()) {
+    document.body.classList.add('_touch')
+
+    const menuArrows = document.querySelectorAll('.menu-arrow')
+
+    if (menuArrows.length > 0) {
+        for (let i = 0; i < menuArrows.length; i++) {
+            const menuArrow = menuArrows[i];
+            menuArrow.addEventListener('click', function(e) {
+                menuArrow.parentElement.classList.toggle('_active');
+            })
+
+        }
+    }
+} else {
+    document.body.classList.add('_pc')
+}
+// Меню бургер
+
+const iconMenu = document.querySelector('.menu-icon');
+if (iconMenu) {
+    const bodyMenu = document.querySelector('.menu-body');
+    iconMenu.addEventListener('click', function(e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        bodyMenu.classList.toggle('_active');
+
+    })
+}
+
+// Прокрутка при клике
+
+const menuLinks = document.querySelectorAll('.menu-link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuClick);
+    });
+
+    function onMenuClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.scrollY - document.querySelector('header').offsetHeight;
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
